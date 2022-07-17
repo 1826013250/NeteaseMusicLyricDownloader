35cd36bad9cd38b65ca929c627ba3320bebac418
1826013250 <62735351+1826013250@users.noreply.github.com>
2022-07-17 18:59:03 +0800

    Add more information about this project
    Add this github link
    Special Thanks added

--- modules/information.py
+++ modules/information.py
@@ -7,10 +7,14 @@ def print_info(self):
     clear()
     print(f"""[NeteaseMusicLyricDownloader Reloaded]
 版本: {self.version}
-本软件开源，项目地址:<url>
+本软件开源，项目地址:https://github.com/1826013250/NeteaseMusicLyricDownloader
 作者:David-123
 联系方式:
-    QQ:1826013250
-    E-mail:1826013250@qq.com(mainly)
-           mc1826013250@gmail.com""")
+\tQQ:1826013250
+\tE-mail:1826013250@qq.com(mainly)
+\t       mc1826013250@gmail.com
+
+Special Thanks:
+\t- chenjunyu19, provided the 163key's cipher
+\t- website 'MKLAB', provided the AES decryption service""")
     input("按回车键返回...")
